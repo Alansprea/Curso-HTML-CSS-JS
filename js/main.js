8299e21bda97956a7611a554672fb11b259e6eef
Alan <alaneduardosprea26@gmail.com>
2022-02-23 10:32:26 -0300

    Inicio

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function(){
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js');
+    $body.classList.add('js');
+
+
+    var $btnMenu = document.querySelector('.header__btnMenu');
+    $btnMenu.removeAttribute('style');
+
+    
+})()
